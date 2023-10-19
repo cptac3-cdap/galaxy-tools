e8f25d5d260e7a65332d39e81e196c113fa9457c
Nathan Edwards <edwardsnj@gmail.com>
2023-10-19 11:49:28 -0400

    CSV module craps out on short peptide that is in many protein sequences

--- msgfplus/fixdoublemassshift.py
+++ msgfplus/fixdoublemassshift.py
@@ -2,13 +2,16 @@
 
 import sys, os, os.path, csv, re
 
-dr = csv.DictReader(sys.stdin,dialect='excel-tab')
-for i,r in enumerate(dr):
-    if i == 0:
-	print "\t".join(dr.fieldnames)
+headers = None
+for l in sys.stdin:
+    sl = l.rstrip().split('\t')
+    if not headers:
+        headers = sl
+        print "\t".join(headers)
+        continue
+    r = dict(zip(headers,sl))
     newpep = []
     spep = re.split(r'([A-Z]+)',r['Peptide'])
-    # print >>sys.stderr, spep
     for i in range(len(spep)):
         if not spep[i]:
             continue
@@ -18,4 +21,4 @@ for i,r in enumerate(dr):
 	    newpep.append("%+.3f"%(eval(spep[i]),))
     # print >>sys.stderr, newpep
     r['Peptide'] = "".join(newpep)
-    print "\t".join(map(r.get,dr.fieldnames))
+    print "\t".join(map(r.get,headers))
